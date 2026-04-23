bda02dd2adf9f86b2de79ca53e8a8b848029c860
AngeloWang_Laptop <angelo.fu.wa@gmail.com>
2026-04-23 11:09:41 +0200

    made danger field walkable

--- src/app/features/game/constants/game-config.ts
+++ src/app/features/game/constants/game-config.ts
@@ -4,7 +4,7 @@ export class GameConfig {
     public static chunkSize: number = 24;
     public static chunkLoadDistance: number = 1;
     public static tileSize = 64;
-    public static walkableTiles = [TileType.GROUND];
+    public static walkableTiles = [TileType.GROUND, TileType.DANGER];
     public static movableTiles = [TileType.GROUND, TileType.DANGER];
     public static immovableTiles = [TileType.WALL];
 }

6696db0037c28c01777b8a353dec04e13ed2076a
AngeloWang_Laptop <angelo.fu.wa@gmail.com>
2026-04-23 10:14:32 +0200

    implemented line jumps, if a player moves horiontally to an empty field, he gets moved to the next left ground tile

--- src/app/features/game/constants/map-data.ts
+++ src/app/features/game/constants/map-data.ts
@@ -2,7 +2,7 @@ export const chunks = new Map<string, string[]>(
     [
         ["0-0",
             [
-                "'H'e'l'l'o' 'P'l'a'y'e'r',$ $ $ |w",
+                "'H'e'l'l'o' 'P'l'a'y'e'r',$ $ $ ",
                 "'T'h'i's' 'i's' 'V'i'M'a'n'i'a'!$ $ $ |w",
                 "|w|w|w|w"
             ]

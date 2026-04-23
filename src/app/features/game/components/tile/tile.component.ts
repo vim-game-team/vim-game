dff48b316a590c694f5b6756f973c8cfb766127b
supjan <supjan@pm.me>
2026-04-23 13:05:50 +0200

    change current tile display

--- src/app/features/game/components/tile/tile.component.ts
+++ src/app/features/game/components/tile/tile.component.ts
@@ -1,16 +1,25 @@
-import { Component, inject, Input } from "@angular/core";
-import { GameState } from "../../services/game-state.service"
-import { Tile } from "../../models/tile";
+import { Component, inject, Input } from '@angular/core';
+import { GameState } from '../../services/game-state.service';
+import { Tile } from '../../models/tile';
 
 @Component({
-    selector: "tile-component",
-    template: `<div id="{{id}}"class="tile {{ tile.type }}">{{tile.value}}</div> `,
-    styleUrl: "./tile.css"
+  selector: 'tile-component',
+  template: `
+    <div [id]="id" class="tile" [class]="tile.type" [class.player]="isPlayerHere()">
+      {{ tile.value }}
+    </div>
+  `,
+  styleUrl: './tile.css',
 })
-
 export class TileComponent {
-    @Input() tile!: Tile;
-    @Input() id!: string;
-    public gameState = inject(GameState);
-    
-};
+  @Input() tile!: Tile;
+  @Input() id!: string;
+  @Input() x!: number;
+  @Input() y!: number;
+
+  public gameState = inject(GameState);
+
+  isPlayerHere(): boolean {
+    return this.gameState.player.posX() === this.x && this.gameState.player.posY() === this.y;
+  }
+}

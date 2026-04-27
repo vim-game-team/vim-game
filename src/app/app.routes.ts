c2c969377a9f93994a597374a80307882fbf4e1a
anuchii <anaflorea1701@icloud.com>
2026-04-27 11:11:51 +0200

    feat: add feature routes and pages components, closes #19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from "./features/game/game";
+import { Login } from './features/login/login';
+import { Register } from './features/register/register';
+import { Profile } from './features/profile/profile';
+import { Commands } from './features/commands/commands';
+import { Intro } from './features/intro/intro';
 
 export const routes: Routes = [
-    { path: "", component: GameComponent }
+    { path: "", component: GameComponent },
+    { path: "login", component: Login },
+    { path: "register", component: Register },
+    { path: "profile", component: Profile },
+    { path: "commands", component: Commands },
+    { path: "intro", component: Intro }
+
 ];

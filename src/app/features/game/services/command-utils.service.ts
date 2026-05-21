1a8345b2843e1ba9bf3f65adf6b3d108c3d3d3b2
supjan <supjan@pm.me>
2026-05-21 10:32:26 +0200

    Add b and B commands

--- src/app/features/game/services/command-utils.service.ts
+++ src/app/features/game/services/command-utils.service.ts
@@ -1,27 +1,68 @@
-import { isAlphaNumeric } from "../../../shared/utils";
-import { Map } from "../models/map";
-import { Player } from "../models/player";
-import { CharType } from "../models/types";
+import { isAlphaNumeric } from '../../../shared/utils';
+import { Map } from '../models/map';
+import { Player } from '../models/player';
+import { CharType } from '../models/types';
 
 export class CmdUtils {
-    public static map: Map;
-    public static player: Player;
+  public static map: Map;
+  public static player: Player;
 
-    public static getCharType(char: string): CharType {
-        switch (true) {
-            case char == " ": return CharType.WHITESPACE;
-            case isAlphaNumeric(char): return CharType.ALPHANUM;
-            default: return CharType.SYMBOL;
-        }
+  public static getCharType(char: string): CharType {
+    switch (true) {
+      case char == ' ':
+        return CharType.WHITESPACE;
+      case isAlphaNumeric(char):
+        return CharType.ALPHANUM;
+      default:
+        return CharType.SYMBOL;
+    }
+  }
+
+  public static offsetToNextNonCharType(type: CharType, offset = 0) {
+    let curTileType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
+    while (curTileType == type) {
+      offset++;
+      curTileType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
     }
 
-    public static offsetToNextNonCharType(type: CharType, offset = 0) {
-        let curTileType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
-        while (curTileType == type) {
-            offset++;
-            curTileType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
-        }
-        
-        return offset
+    return offset;
+  }
+
+  public static offsetToPrevWhitespace(offset = 0) {
+    let curTileType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
+    while (curTileType !== CharType.WHITESPACE) {
+      offset--;
+      curTileType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
     }
+
+    return offset;
+  }
+
+  public static offsetToPrevNonCharType(type: CharType, offset = 0) {
+    let curTileType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
+    while (curTileType === type) {
+      offset--;
+      curTileType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
+    }
+
+    return offset;
+  }
+
+  public static offsetToPrevWordStart(type: CharType, offset = 0): number {
+    let curType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
+    if (curType === type || curType === CharType.WHITESPACE) {
+      while (
+        this.getCharType(this.player.relativeTileAt(offset, 0).value) === CharType.WHITESPACE
+      ) {
+        offset--;
+      }
+      curType = this.getCharType(this.player.relativeTileAt(offset, 0).value);
+    }
+
+    while (this.getCharType(this.player.relativeTileAt(offset - 1, 0).value) === curType) {
+      offset--;
+    }
+
+    return offset;
+  }
 }
--- src/app/features/game/services/command.service.ts
+++ src/app/features/game/services/command.service.ts
@@ -1,73 +1,121 @@
-import { GameState } from "./game-state.service";
-import { InputToken } from "../models/inputToken";
-import { Command } from "../../../shared/models/command";
-import { CharType, CmdType, InputMode } from "../models/types";
-import { CmdUtils } from "./command-utils.service";
+import { GameState } from './game-state.service';
+import { InputToken } from '../models/inputToken';
+import { Command } from '../../../shared/models/command';
+import { CharType, CmdType, InputMode } from '../models/types';
+import { CmdUtils } from './command-utils.service';
 
 export class CommandService {
-    public gameState: GameState;
+  public gameState: GameState;
 
-    public constructor(gameState: GameState) {
-        this.gameState = gameState;
-        CmdUtils.map = gameState.map;
-        CmdUtils.player = gameState.player;
-    }
+  public constructor(gameState: GameState) {
+    this.gameState = gameState;
+    CmdUtils.map = gameState.map;
+    CmdUtils.player = gameState.player;
+  }
 
+  public execute(tokens: InputToken[]) {
+    let action: CmdType = tokens[0].cmd.type as CmdType;
+    const executeFunction = (this as any)['execute_' + tokens[0].cmd.key];
 
-    public execute(tokens: InputToken[]) {
-        let action: CmdType = tokens[0].cmd.type as CmdType;
-        const executeFunction = (this as any)["execute_" + tokens[0].cmd.key];
-
-        try {
-            switch (action) {
-                case CmdType.MOVEMENT: {
-                    let result: any = executeFunction.call(this);
-                    for (let i = 0; i < tokens[0].count; i++)
-                        this.gameState.player.move(result[0], result[1]);
-                    break;
-                }
-                case CmdType.MODESWITCH: {
-                    let result: any = executeFunction.call(this);
-                    break;
-                }
-            }
+    try {
+      switch (action) {
+        case CmdType.MOVEMENT: {
+          let result: any = executeFunction.call(this);
+          for (let i = 0; i < tokens[0].count; i++)
+            this.gameState.player.move(result[0], result[1]);
+          break;
         }
-        catch (e) {
-
+        case CmdType.MODESWITCH: {
+          let result: any = executeFunction.call(this);
+          break;
         }
+      }
+    } catch (e) {}
+  }
+
+  private execute_h() {
+    return [-1, 0];
+  }
+
+  private execute_j() {
+    return [0, 1];
+  }
+
+  private execute_k() {
+    return [0, -1];
+  }
+
+  private execute_l() {
+    return [1, 0];
+  }
+
+  private execute_w(count1: number, cmd: Command) {
+    let curChar = this.gameState.player.curTile().value;
+    let curCharType: CharType = CmdUtils.getCharType(curChar);
+
+    let offset = CmdUtils.offsetToNextNonCharType(curCharType);
+    offset = CmdUtils.offsetToNextNonCharType(CharType.WHITESPACE, offset);
+
+    return [offset, 0];
+  }
+
+  private execute_i() {
+    this.gameState.inputMode = InputMode.INSERT;
+  }
+
+  private execute_a() {
+    this.gameState.inputMode = InputMode.INSERT;
+  }
+
+  // moves to the start of the current word
+  // if on whitespace or already at a word start -> skip back over whitespace -> then go to start of previous word
+  // word = block of same char type (alphanum or symbol)
+  private execute_b() {
+    const curChar = this.gameState.player.curTile().value;
+    let curType = CmdUtils.getCharType(curChar);
+    let offset = 0;
+
+    const prevType = CmdUtils.getCharType(
+      this.gameState.player.relativeTileAt(offset - 1, 0).value,
+    );
+    if (curType === CharType.WHITESPACE || prevType !== curType) {
+      while (
+        CmdUtils.getCharType(this.gameState.player.relativeTileAt(offset - 1, 0).value) ===
+        CharType.WHITESPACE
+      ) {
+        offset--;
+      }
+      curType = CmdUtils.getCharType(this.gameState.player.relativeTileAt(offset - 1, 0).value);
     }
 
-    private execute_h() {
-        return [-1, 0];
+    while (
+      CmdUtils.getCharType(this.gameState.player.relativeTileAt(offset - 1, 0).value) === curType
+    ) {
+      offset--;
     }
 
-    private execute_j() {
-        return [0, 1];
+    return [offset, 0];
+  }
+
+  // moves to the start of the current WORD
+  // WORD = block of non-whitespace (ignores symbol/alphanum distinction)
+  // if on whitespace or at a WORD start -> skip back over whitespace -> then go to start of previous WORD
+  private execute_B() {
+    let offset = 0;
+    while (
+      CmdUtils.getCharType(this.gameState.player.relativeTileAt(offset - 1, 0).value) ===
+      CharType.WHITESPACE
+    ) {
+      offset--;
     }
 
-    private execute_k() {
-        return [0, -1];
+    while (
+      CmdUtils.getCharType(this.gameState.player.relativeTileAt(offset - 1, 0).value) !==
+      CharType.WHITESPACE
+    ) {
+      offset--;
     }
 
-    private execute_l() {
-        return [1, 0];
-    }
-
-    private execute_w(count1: number, cmd: Command) {
-        let curChar = this.gameState.player.curTile().value;
-        let curCharType: CharType = CmdUtils.getCharType(curChar);
-        
-        let offset = CmdUtils.offsetToNextNonCharType(curCharType);
-        offset = CmdUtils.offsetToNextNonCharType(CharType.WHITESPACE, offset);
-
-        return [offset, 0];
-    }
-
-    private execute_i() {
-        this.gameState.inputMode = InputMode.INSERT;
-    }
-
-    private execute_a() {
-        this.gameState.inputMode = InputMode.INSERT;
-    }
+    return [offset, 0];
+  }
 }

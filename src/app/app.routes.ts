44ea1c4d81e43ad2492ff9e57f9246aa056c1763
Ana <anaflorea1701@icloud.com>
2026-06-09 20:13:18 +0200

    feat(auth) protect game, profile and commands routes with auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,14 @@ import { Profile } from './features/profile/profile';
 import { Commands } from './features/commands/commands';
 import { Intro } from './features/intro/intro';
 import { NotFound } from './not-found/not-found';
+import { authGuard } from './shared/guards/auth.guard'; 
 
 export const routes: Routes = [
-    { path: "", component: GameComponent },
+    { path: "", component: GameComponent, canActivate: [authGuard] },
     { path: "login", component: Login },
     { path: "register", component: Register },
-    { path: "profile", component: Profile },
-    { path: "commands", component: Commands },
+    { path: "profile", component: Profile, canActivate:[authGuard] },
+    { path: "commands", component: Commands, canActivate: [authGuard] },
     { path: "intro", component: Intro },
     { path: "**", component: NotFound }
 
